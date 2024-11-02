11c8e87fb5e3dbf02681122506ea1499642d10be
BrockBadeaux14 <59292824+BrockBadeaux14@users.noreply.github.com>
2024-11-01 23:55:48 -0500

    I found the mistake

--- script.js
+++ script.js
@@ -4,14 +4,14 @@ const foodData = [
         ingredients: "Bread, ham, linguiça, fresh sausage, steak, cheese, etc.",
         description: "Layers of bread, ham, sausage, steak with melted cheese...",
         country: "Portugal",
-        image: "./images/francesinha.jpg"
+        image: "./Images/francesinha.jpg"
     },
     {
         name: "Bibimbap",
         ingredients: "Rice, beef, assorted vegetables (like spinach, bean sprouts, carrots), gochujang (Korean chili paste), soy sauce, sesame oil, fried egg (optional).",
         description: "A Korean dish that consists of a bowl of warm rice topped with sautéed vegetables, sliced meat, and a raw or fried egg. It's typically served with gochujang, which is mixed in right before eating for a spicy kick.",
         country: "South Korea",
-        image: "./images/bibimap.jpg" 
+        image: "./Images/bibimap.jpg" 
     },
     
 ];
